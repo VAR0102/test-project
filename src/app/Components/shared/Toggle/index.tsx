5de1a45eaba67c03013c9a2b873fc9bc1195cf9d
Varuj Tsaturyan <varujtsaturyan@Varujs-MacBook-Pro.local>
2025-09-10 21:37:46 +0400

    save new changes in tab

--- src/app/Components/shared/Toggle/index.tsx
+++ src/app/Components/shared/Toggle/index.tsx
@@ -1,42 +1,28 @@
 "use client";
-import React, { ReactNode, useState } from "react";
-
 interface ToggleProps {
-  className?: string;
-  children?: ReactNode;
   disabled?: boolean;
-  onChange: (value: boolean) => void; 
+  isChecked: boolean;
+  onChange: (value: boolean) => void;
 }
 
 const Toggle = ({
-  className = "",
-  children,
   disabled = false,
   onChange,
+  isChecked,
 }: ToggleProps) => {
-  const [isOn, setIsOn] = useState(false);
-
-  const handleClick = () => {
-    if (disabled) 
-      return;
-    setIsOn(!isOn);
-    onChange(!isOn); 
-  };
-
   return (
     <button
-      onClick={handleClick}
+      onClick={() => onChange(isChecked)}
       disabled={disabled}
-      className={`relative cursor-pointer flex items-center transition-all duration-300
-        ${isOn ? "w-[60px] h-[30px] bg-[#3C5A4B] rounded-[20px]" : "w-[60px] h-[30px] bg-[#E6E6E6] rounded-[20px]"}
-        ${disabled ? "cursor-not-allowed bg-gray-200" : ""} ${className}`}
+      className={`relative cursor-pointer flex w-[60px] h-[30px] rounded-[20px] items-center transition-all duration-300
+        ${isChecked ? "bg-[#3C5A4B]" : "bg-[#E6E6E6]"}
+        ${disabled ? "cursor-not-allowed bg-gray-200" : ""}`}
     >
       <span
         className={`w-[22px] h-[22px] bg-white rounded-full transition-all duration-300 ${
-          isOn ? "ml-[34px]" : "ml-1"
+          isChecked ? "ml-[34px]" : "ml-1"
         }`}
-      ></span>
-      {children}
+      />
     </button>
   );
 };
